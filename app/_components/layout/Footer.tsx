7617c1f1ff60bebb716b89497282f66cbb3eb19d
uzairirfanofficeai-netizen <uzairirfan.officeai@gmail.com>
2026-05-13 16:29:34 +0500

    changes

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -152,7 +152,7 @@ function DesktopFooter() {
           </div>
 
           <div className="flex flex-col gap-[20px] sm:order-5 sm:col-span-2 lg:col-span-1">
-            <div className="flex h-[32px] items-center gap-[22px]">
+            <div className="flex items-center gap-[14px]">
               <SocialLink label="Facebook">
                 <FacebookIcon />
               </SocialLink>
@@ -166,7 +166,15 @@ function DesktopFooter() {
                 <InstagramIcon />
               </SocialLink>
             </div>
-            <RateMyAgentBadge />
+            <Image
+              src="/images/footer%20image.png"
+              alt="Rate My Agent"
+              width={1076}
+              height={324}
+              quality={100}
+              sizes="180px"
+              className="h-auto w-[180px]"
+            />
           </div>
         </div>
       </div>
@@ -239,43 +247,32 @@ function SocialLink({
     <a
       href="#"
       aria-label={label}
-      className="inline-flex h-[28px] w-[28px] items-center justify-center text-brand-navy transition hover:opacity-70"
+      className="inline-flex h-[36px] w-[36px] items-center justify-center rounded-full bg-black text-white transition hover:opacity-80"
     >
       {children}
     </a>
   );
 }
 
-function RateMyAgentBadge() {
-  return (
-    <div className="inline-flex h-[48px] w-[180px] items-center justify-center rounded-[6px] bg-brand-navy px-4 font-display text-[18px] font-bold tracking-tight text-white">
-      <span>rate</span>
-      <span className="mx-[2px] text-rose-500">my</span>
-      <span>agent</span>
-      <span className="ml-[2px] -translate-y-[6px] text-[9px]">&reg;</span>
-    </div>
-  );
-}
-
 function FacebookIcon() {
   return (
-    <svg viewBox="0 0 24 24" fill="currentColor" className="h-[26px] w-[26px]" aria-hidden>
-      <path d="M24 12.07C24 5.41 18.63 0 12 0S0 5.41 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.05V9.41c0-3.02 1.79-4.69 4.53-4.69 1.31 0 2.69.24 2.69.24v2.97h-1.51c-1.49 0-1.96.93-1.96 1.89v2.26h3.33l-.53 3.49h-2.8V24C19.61 23.1 24 18.1 24 12.07z" />
+    <svg viewBox="0 0 24 24" fill="currentColor" className="h-[18px] w-[18px]" aria-hidden>
+      <path d="M13.5 22v-8h2.7l.4-3.1h-3.1V8.9c0-.9.25-1.5 1.55-1.5H16.7V4.65A22 22 0 0 0 14.4 4.5c-2.3 0-3.9 1.4-3.9 4v2.4H7.8V14h2.7v8h3z" />
     </svg>
   );
 }
 
 function YouTubeIcon() {
   return (
-    <svg viewBox="0 0 24 24" fill="currentColor" className="h-[22px] w-[30px]" aria-hidden>
-      <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+    <svg viewBox="0 0 24 24" fill="currentColor" className="h-[20px] w-[20px]" aria-hidden>
+      <path d="M21.6 7.2a2.5 2.5 0 0 0-1.76-1.77C18.27 5 12 5 12 5s-6.27 0-7.84.43A2.5 2.5 0 0 0 2.4 7.2 26 26 0 0 0 2 12a26 26 0 0 0 .4 4.8 2.5 2.5 0 0 0 1.76 1.77C5.73 19 12 19 12 19s6.27 0 7.84-.43a2.5 2.5 0 0 0 1.76-1.77A26 26 0 0 0 22 12a26 26 0 0 0-.4-4.8zM10 15.5v-7l6 3.5-6 3.5z" />
     </svg>
   );
 }
 
 function TikTokIcon() {
   return (
-    <svg viewBox="0 0 24 24" fill="currentColor" className="h-[26px] w-[26px]" aria-hidden>
+    <svg viewBox="0 0 24 24" fill="currentColor" className="h-[18px] w-[18px]" aria-hidden>
       <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5.8 20.1a6.34 6.34 0 0 0 10.86-4.43V8.69a8.16 8.16 0 0 0 4.77 1.52V6.81a4.85 4.85 0 0 1-1.84-.12z" />
     </svg>
   );
@@ -283,7 +280,7 @@ function TikTokIcon() {
 
 function InstagramIcon() {
   return (
-    <svg viewBox="0 0 24 24" fill="currentColor" className="h-[26px] w-[26px]" aria-hidden>
+    <svg viewBox="0 0 24 24" fill="currentColor" className="h-[18px] w-[18px]" aria-hidden>
       <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z" />
     </svg>
   );
