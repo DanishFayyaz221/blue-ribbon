7ae447392a613587233e5ca4ecec1bc5adf5e546
uzairirfanofficeai-netizen <uzairirfan.officeai@gmail.com>
2026-06-03 14:51:04 +0500

    changes

--- app/_components/home/Hero.tsx
+++ app/_components/home/Hero.tsx
@@ -126,8 +126,8 @@ function SearchBar() {
           </label>
           <button type="button" aria-label="Filters" className="text-black hover:opacity-70">
             <svg
-              viewBox="0 0 24 21"
-              className="h-[16px] w-[18px] lg:h-[18px] lg:w-[21px]"
+              viewBox="0 0 24 24"
+              className="h-[18px] w-[20px] lg:h-[20px] lg:w-[22px]"
               fill="none"
               stroke="currentColor"
               strokeWidth="2"
@@ -135,9 +135,15 @@ function SearchBar() {
               strokeLinejoin="round"
               aria-hidden
             >
-              <line x1="3" y1="6" x2="21" y2="6" />
-              <line x1="6" y1="11" x2="18" y2="11" />
-              <line x1="9" y1="16" x2="15" y2="16" />
+              <line x1="21" y1="6" x2="14" y2="6" />
+              <line x1="10" y1="6" x2="3" y2="6" />
+              <line x1="21" y1="12" x2="12" y2="12" />
+              <line x1="8" y1="12" x2="3" y2="12" />
+              <line x1="21" y1="18" x2="16" y2="18" />
+              <line x1="12" y1="18" x2="3" y2="18" />
+              <line x1="14" y1="4" x2="14" y2="8" />
+              <line x1="8" y1="10" x2="8" y2="14" />
+              <line x1="16" y1="16" x2="16" y2="20" />
             </svg>
           </button>
         </div>
